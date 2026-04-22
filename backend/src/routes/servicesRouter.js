3f5c59b2dc50dd38522b6007855f30929f29f622
Cao Giang Huy <caogianghuy1305@gmail.com>
2026-04-22 23:28:09 +0700

    đi ngủ thôi mấy cvo ơi

--- backend/src/routes/servicesRouter.js
+++ backend/src/routes/servicesRouter.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const servicesController = require("../controllers/servicesController");
+const { verifyToken, isTourStaff } = require("../middlewares/auth");
 
-router.get("/", servicesController.getAllServices);
-router.get("/:id", servicesController.getServiceById);
-router.post("/", servicesController.createService);
-router.put("/:id", servicesController.updateService);
-router.delete("/:id", servicesController.deleteService);
+router.get("/", verifyToken, isTourStaff, servicesController.getAllServices);
+router.get("/:id", verifyToken, isTourStaff, servicesController.getServiceById);
+router.post("/", verifyToken, isTourStaff, servicesController.createService);
+router.put("/:id", verifyToken, isTourStaff, servicesController.updateService);
+router.delete("/:id", verifyToken, isTourStaff, servicesController.deleteService);
 
 module.exports = router;
